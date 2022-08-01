0be8c72f099613bc1b89717a66adffb86fd6767e
Mitali2700 <201B155@juetguna.in>
2022-08-01 22:26:27 +0530

    printing entire javascript object in the hyper terminal

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+const { response } = require("express");
 const express = require("express");
 const https = require("https");
 const app = express();
@@ -7,7 +8,15 @@ app.get("/",function(req, res){
     const url ="https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=1e05b64943c967b43b3d8f9404a7c5f0&units=metric";
     https.get(url, function(response){
         console.log(response.statusCode);
+
+        response.on("data", function(data){
+            // console.log(data);
+            const weatherData = JSON.parse(data);
+            console.log(weatherData);
+        })
+    
     })
+    
     
     res.send("Server is up and running.")
 })
