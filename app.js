5effef6917373d05dc482d2d51f9a2168647e5b7
Mitali2700 <201B155@juetguna.in>
2022-08-01 18:07:07 +0530

    making http request in node

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const express = require("express");
 const app = express();
 
 app.get("/",function(req, res){
+    const url ="https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=1e05b64943c967b43b3d8f9404a7c5f0&units=metric";
+    https.get(url, function(response){
+        console.log(response);
+    });
+    
     res.send("Server is up and running.")
 })
 
