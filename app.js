308f55f2c748f304e38c084d1775bfae7365dc55
Mitali2700 <201B155@juetguna.in>
2022-08-03 13:30:44 +0530

    adding bodyParser

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-
-const { response } = require("express");
+//const { response } = require("express");
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
+
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req, res){
   res.sendFile(__dirname+ "/index.html")
 })
 
-
+app.post("/", function(req, res){
+   console.log(req.body.cityName);
+   //console.log("Post request recieved.")
+})
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
